54d057d861b643255bc771a50b2eed1e7aef582b
Vinícius Leite <153463870+viniXLeite@users.noreply.github.com>
2024-05-18 15:29:55 -0300

    Update NULLConcept.c

--- Pointers/NULLConcept.c
+++ Pointers/NULLConcept.c
@@ -4,7 +4,7 @@
 
 int main() {
     int number = 10;
-    int* pi = NULL; // pi do not pointers to any memory address, we could also write pi = 0; in the next line instead or int *pi = 0;
+    int* pi = NULL; // pi do not point to any memory address, we could also write pi = 0; in the next line instead or int *pi = 0;
                     // *pi = 0; this one would refer to the 0 integer
     int ** pi_2 = &pi; // pi is storaged in some part of the memory, yet it does not point to any value 
     if (pi == NULL) 
