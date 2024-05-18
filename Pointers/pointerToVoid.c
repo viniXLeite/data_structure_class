141de1af0513e938728c624029c356321cc0687f
Vinícius Leite <153463870+viniXLeite@users.noreply.github.com>
2024-05-18 15:27:22 -0300

    Update pointerToVoid.c

--- Pointers/pointerToVoid.c
+++ Pointers/pointerToVoid.c
@@ -3,7 +3,7 @@
 int main() {
     int num;
     int *pi = &num;
-    void* pv = &num; // a pointer to void can pointer to any type of data
+    void* pv = &num; // a pointer to void can point to any type of data
 
     printf("Value of pi: %p\n", pi);
     printf("Size of pv: %lu\n", sizeof(void *)); // a pointer to void will always have the same alignment as a pointer to char
