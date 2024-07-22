fe4b5e55154408f8a4eda149d7815a3f3002b1b0
Vinícius Leite <153463870+viniXLeite@users.noreply.github.com>
2024-07-22 17:24:32 -0300

    update: read_write.c

--- doubly_linked_list/read.c
+++ doubly_linked_list/read.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
-int main() {
-	FILE* input = fopen("entrada", "r");
-	FILE* output = fopen("saida", "w");
+int main(int argc, char* argv[]) {
+	FILE* input = fopen(argv[1], "r");
+	FILE* output = fopen(argv[2], "w");
 
 
     // Store the content of the file
