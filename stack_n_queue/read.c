06200a854115a8bc72d6b40457040d3c8357fc30
Vinícius Leite <153463870+viniXLeite@users.noreply.github.com>
2024-08-03 16:17:47 -0300

    fix: read.c

--- stack_n_queue/read.c
+++ stack_n_queue/read.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
+#include <ctype.h>
 
 typedef struct _Printers {
     int linesNumber;
@@ -16,8 +16,7 @@ Printers getPrinters(FILE* input) {
     Printers Printers;
 
     if(fgets(commandLine, sizeof(commandLine), input)) {
-        char charCommandsNumber = commandLine[0];
-        int commandsNumber = charCommandsNumber - '0';
+        int commandsNumber = atoi(commandLine);
 
         Printers.linesNumber = commandsNumber;
         Printers.linesArray = malloc(commandsNumber*sizeof(char*)+1);
@@ -37,24 +36,68 @@ Printers getPrinters(FILE* input) {
 
 //int get_docPages()
 
+int is_number(const char *str) {
+    while (*str) {
+        if (!isdigit(*str)) return 0;
+        str++;
+    }
+    return 1;
+}
+
+Printers read_lines_based_on_number(FILE* input, FILE* output) {
+    Printers Printers;
+
+    if (input == NULL) {
+        perror("Erro ao abrir o arquivo");
+        exit(EXIT_FAILURE);
+    }
+
+    char line[100];
+    if (fgets(line, sizeof(line), input)) {
+        line[strcspn(line, "\n")] = '\0';
+        fprintf(output,"%s\n", line);
+        if (is_number(line)) {
+            int count = atoi(line);
+            Printers.linesNumber = count;
+            Printers.linesArray = (char**) malloc(Printers.linesNumber*51);
+
+            for (int i = 0; i < count; i++) {
+                if (fgets(line, sizeof(line), input)) {
+                    // Remove o caractere de nova linha, se presente
+                    line[strcspn(line, "\n")] = '\0';
+                    Printers.linesArray[i] = (char*) malloc(strlen(line)+1);
+                    strcpy(Printers.linesArray[i], line);
+                    fprintf(output,"%s\n", Printers.linesArray[i]);
+                }
+            }
+        } else {
+            printf("A primeira linha do arquivo não é um número válido.\n");
+        }
+    }
+
+    return Printers;
+
+}
+
 
 int main(int argc, char* argv[]) {
 	FILE* input = fopen(argv[1], "r");
 	FILE* output = fopen(argv[2], "w");
 
-    Printers printers = getPrinters(input);
-    fprintf(output, "%d\n", printers.linesNumber);
-    for(int i = 0; i <= printers.linesNumber-1; i++) {
-        fprintf(output, "%s\n", printers.linesArray[i]);
-    }
+    Printers printers = read_lines_based_on_number(input, output);
+    Printers Documents = read_lines_based_on_number(input, output);
+    //fprintf(output, "%d\n", printers.linesNumber);
+    //for(int i = 0; i <= printers.linesNumber-1; i++) {
+      //  fprintf(output, "%s\n", printers.linesArray[i]);
+    //}
 
-    printf("\n");
+    //printf("\n");
 
-    Printers documents = getPrinters(input);
-    fprintf(output, "\n%d\n", documents.linesNumber);
-    for(int i = 0; i <= documents.linesNumber-1; i++) {
-        fprintf(output, "%s\n", documents.linesArray[i]);
-    }
+    //Printers documents = getPrinters(input);
+    //fprintf(output, "\n%d\n", documents.linesNumber);
+    //for(int i = 0; i <= documents.linesNumber-1; i++) {
+        //fprintf(output, "%s\n", documents.linesArray[i]);
+    //}
 
     // to get the number of pages on a document use documents.linesArray[n][size(documents.linesArray[n])] 
     //and after that convert it integer
