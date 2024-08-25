eb35bb5350c83e7f55da4c08f30f0b4fd2ca851d
Vinícius Leite <153463870+viniXLeite@users.noreply.github.com>
2024-08-25 19:49:14 -0300

    add: sauce binary tree file

--- binary_tree/tree.cpp
+++ binary_tree/tree.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+unsigned int insertion_order = 0;
+
 struct Doc {
     string* name;
     string* type;
@@ -22,9 +24,13 @@ class TreeNode {
     
     TreeNode(): Name(""), left(nullptr), right(nullptr) {}
 
-    void insert_node(string name) {
+    void insert_node(string name, string type, unsigned int size) {// add more parameters 
         if (this->Name.empty()) {
             this->Name = name;
+            this->type = type;
+            this->size = size;
+            this->order = insertion_order;
+            insertion_order++;
             return;
         }
 
@@ -37,51 +43,69 @@ class TreeNode {
                 } else {
                     current->left = new TreeNode();
                     current->left->Name = name;
+                    current->left->type = type;
+                    current->left->size = size;
+                    current->left->order = insertion_order;
+                    insertion_order++;
                     break;
                 }
-            //else if(name == current->Name) do not change the pointers only the size, name and type
-            } else {
+            }
+            else if(name == current->Name && current->type == "rw") {
+                current->type = type;
+                current->order = insertion_order;
+                current->size = size;
+                insertion_order++;
+                break;
+            }
+            else {
                 if (current->right != nullptr) {
                     current = current->right;
                 } else {
                     current->right = new TreeNode();
                     current->right->Name = name;
+                    current->right->type = type;
+                    current->right->size = size;
+                    current->right->order = insertion_order;
+                    insertion_order++;
                     break;
                 }
             }
         }
     }
 
-    void print_in_order() {
+    void print_in_order(ofstream &output) {
         if(left != nullptr) {
-            left->print_in_order();
+            left->print_in_order(output);
         }
-        cout << Name << endl;
+        if (size > 1) output << order << " " << Name << " " << type << " " << size << " bytes" << endl;
+        else output << order << " " << Name << " " << type << " " << size << " byte" << endl;
         if(right != nullptr) {
-            right->print_in_order();
+            right->print_in_order(output);
         }
     }
 
-    void preOrder() {
-        cout << Name << endl;
+    void preOrder(ofstream &output) {
+        if (size > 1) output << order << " " << Name << " " << type << " " << size << " bytes" << endl;
+        else output << order << " " << Name << " " << type << " " << size << " byte" << endl;        
+        
         if(left != nullptr) {
-            left->preOrder();
+            left->preOrder(output);
         }
         if(right != nullptr) {
-            right->preOrder();
+            right->preOrder(output);
         }
     }
 
-    void postOrder() {
+    void postOrder(ofstream &output) {
         if(left != nullptr) {
-            left->postOrder();
+            left->postOrder(output);
         }
         if(right != nullptr) {
-            right->postOrder();
+            right->postOrder(output);
         }
-        cout << Name << endl;
+        if (size > 1) output << order << " " << Name << " " << type << " " << size << " bytes" << endl;
+        else output << order << " " << Name << " " << type << " " << size << " byte" << endl;
     }
-
 };
 
 Doc get_docs(ifstream &input, unsigned int number_of_docs) {
@@ -90,7 +114,7 @@ Doc get_docs(ifstream &input, unsigned int number_of_docs) {
     doc.type = new string[number_of_docs];
     doc.size = new unsigned int[number_of_docs];
 
-    for(int i = 0; i < number_of_docs; i++) {
+    for(unsigned int i = 0; i < number_of_docs; i++) {
         input >> doc.name[i] >> doc.type[i] >> doc.size[i];
     }
     return doc;
@@ -103,19 +127,21 @@ int main(int argc, char* argv[]) {
     unsigned int number_of_docs;
     input >> number_of_docs;
     Doc doc = get_docs(input, number_of_docs);
+    cout << "OK get_docs" << endl;
 
     TreeNode root;
 
-    for(int i = 0; i < number_of_docs; i++) {
-        root.insert_node(doc.name[i]); // remember to also insert the root.type and the root.size in the tree.insert function
+    for(unsigned int i = 0; i < number_of_docs; i++) {
+        root.insert_node(doc.name[i], doc.type[i], doc.size[i]); // root.inset_node(doc.name[i], doc.size[i], doc.type[i])
     }
+    cout << "OK insert nodes" << endl;
 
-    cout << "EPD:" << endl;
-    root.print_in_order();
-    cout << "PED:" << endl;
-    root.preOrder();
-    cout << "EDP:" << endl;
-    root.postOrder();
+    output << "EPD:" << endl;
+    root.print_in_order(output);
+    output << "PED:" << endl;
+    root.preOrder(output);
+    output << "EDP:" << endl;
+    root.postOrder(output);
 
     return 0;
 }
